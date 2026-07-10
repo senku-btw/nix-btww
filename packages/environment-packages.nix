b66cc44e5049596ba4f33b36dcd875daefa72bb0
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-10 18:53:08 +0000

    Update environment-packages.nix

--- packages/environment-packages.nix
+++ packages/environment-packages.nix
@@ -1,9 +1,16 @@
-# ~/nix-btww/packages/environment-packages.nix
 { pkgs, ... }:
 
 {
   home.packages = with pkgs; [
+    # Desktop Environment / Window Manager tools
     bemenu  # The application launcher
     foot    # The terminal emulator
+    wget
+    curl
+    git
+    nano
+    btop
+    tree
+    fastfetch
   ];
 }
