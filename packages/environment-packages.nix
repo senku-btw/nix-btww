c6dddc6034b405f2700280a11cd6fd1d79953fbb
Senku Ishigami <297473694+senku-btw@users.noreply.github.com>
2026-07-11 14:19:24 +0000

    Update environment-packages.nix

--- packages/environment-packages.nix
+++ packages/environment-packages.nix
@@ -4,6 +4,7 @@
   home.packages = [
     pkgs.bemenu        # Dynamic menu and application launcher
     pkgs.foot          # Fast, lightweight Wayland terminal emulator
+    pkgs.awww          # Animated wallpaper daemon for Wayland
     pkgs.btop          # Interactive system resource monitor
     pkgs.tree          # Directory structure visualizer
     pkgs.fastfetch     # System information display client
